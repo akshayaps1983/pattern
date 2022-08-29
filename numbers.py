37f7fe7ccead7c1769b96da7eb9ad10ebe7517e5
akshayaps1983 <110896904+akshayaps1983@users.noreply.github.com>
2022-08-29 15:34:18 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+    for j in range(i):
+        print(i,end="")
+    print("\r")
+print("----------")
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("\r")
